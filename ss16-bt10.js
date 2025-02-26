97047ddfe60b959c3112f572cbe9a7fcd208a7e4
minh2307 <minh23923@gmail.com>
2025-02-26 17:04:15 +0700

    done

--- ss16-bt10.js
+++ ss16-bt10.js
@@ -1,13 +1,16 @@
 let firstNumber = +prompt(`Mời bạn nhập số đầu tiên`);
 let secondNumber = +prompt(`Mời bạn nhập số thứ hai`);
 let temp;
-if(firstNumber < secondNumber){
-    temp = firstNumber;
-    firstNumber = secondNumber;
-    secondNumber = temp;
+if (firstNumber < secondNumber) {
+  temp = firstNumber;
+  firstNumber = secondNumber;
+  secondNumber = temp;
 }
 let signal = firstNumber - secondNumber;
 let variable = Math.random() * signal;
-alert(variable);
 let ramdom = variable + secondNumber;
-alert(`số ngẫu nhiên nằm trong ${secondNumber}, ${firstNumber} : ${ramdom.toFixed(2)}`);
+alert(
+  `số ngẫu nhiên nằm trong ${secondNumber}, ${firstNumber} : ${ramdom.toFixed(
+    2
+  )}`
+);
--- ss16-bt8.js
+++ ss16-bt8.js
@@ -2,13 +2,11 @@ let fistNumber = +prompt("Mời bạn nhập số thứ nhất");
 let secondNumber = +prompt("Mời bạn nhập số thứ hai");
 let thirdNumber = +prompt("Mời bạn nhập số thứ ba");
 let compare;
-if(fistNumber > secondNumber && fistNumber > thirdNumber){
-    compare = fistNumber;
+if (fistNumber > secondNumber && fistNumber > thirdNumber) {
+  compare = fistNumber;
+} else if (fistNumber < thirdNumber && secondNumber < thirdNumber) {
+  compare = thirdNumber;
+} else {
+  compare = secondNumber;
 }
-else if(fistNumber < thirdNumber && secondNumber < thirdNumber){
-    compare = thirdNumber;
-}
-else{
-    compare = secondNumber;     
-}
-    document.write(`Số lớn nhất là ${compare}`);
+document.write(`Số lớn nhất là ${compare}`);
